b16a75ab187f93cef3f0aeb9bd4645b39d6ce76a
Vlad <example@gmail.com>
2022-12-11 20:30:19 +0200

    conditional statements

--- scrtip.js
+++ scrtip.js
@@ -1,4 +1,38 @@
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('На сколько оцените его?', '');
+"use strict"
+
+if (4 == 9){
+    console.log('Ok!')
+}else{
+console.log('Eror');
+}
+
+
+
+// if(num <49){
+//     console.log('Eror');
+// }else if (num >100){
+//      console.log('Много');
+// }else{
+//     console.log('Ok!');
+// }
+
+// (num === 50) ? console.log('Ok!') : console.log('Eror');
+
+const num = "50";
+
+//switch
+
+switch(num){
+    case 49:
+        console.log('Неверно');
+        break;
+        case 100:
+        console.log('Неверно');
+        break;
+        case '50':
+            console.log('в точку'  );
+        break;
+    default:
+        console.log('Не сегодня ');
+        break;
+}
