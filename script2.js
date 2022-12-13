896a1b72fa64be30c99556a50ab692d1dcfb6d03
Vlad <example@gmail.com>
2022-12-13 22:11:31 +0200

    function

--- script2.js
+++ script2.js
@@ -35,3 +35,48 @@ if(personalMovieDb.count < 10){
     console.log("Произошла ошибка напишите пожалуста цифрами")
 }
 console.log(personalMovieDb);
+
+
+let num = 20;
+
+function showFirstMessage(text){
+   console.log(text);
+   let num = 10;
+   console.log(num);
+}
+showFirstMessage("Hello World!");
+console.log(num);
+
+//++//
+function calc(a,b){
+    return(a + b);
+}
+
+console.log(calc(4,3));
+console.log(calc(5,6));
+console.log(calc(10,6));
+
+//
+function ret(){
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+cons.log(anotherNum);
+
+//
+
+const logger = function(){
+console.log("Hello");
+
+};
+logger();
+
+//
+const calc = (a,b) =>  a + b;
+
+const calc = (a,b) =>  {
+    console.log('1');
+    return a + b;
+};
