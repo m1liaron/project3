9820ab370a0eb6d6cf790df0b6a3119905b815eb
Vlad <example@gmail.com>
2022-12-17 23:54:31 +0200

    callback

--- script2.js
+++ script2.js
@@ -132,3 +132,30 @@ promotion(res);
 
 // convert(500, usdCurr); 
 // convert(500, eurCurr);  
+
+//lessons callback
+
+function first(){
+    //Do
+    setTimeout(function () {
+        console.log(1);
+    },500);
+}
+
+function second(){
+    console.log(2);     
+}
+
+first();
+second();
+
+function  learnJS(lang, callback){
+  console.log(`Я учу:${lang}`);
+  callback();
+}
+
+function done() {
+    console.log('Я прошёл этот урок!');  
+}
+
+learnJS('JavaScript',done);
--- scriptHW.js
+++ scriptHW.js
@@ -275,3 +275,80 @@ function getCoupeNumber(seatNumber){
 getCoupeNumber(33);
 
 
+//№8 
+//1.
+
+function getTimeFromMinutes(minutesTotal) {
+    if (typeof(minutesTotal) !== 'number' || minutesTotal < 0 || !Number.isInteger(minutesTotal)) {
+        return "Ошибка, проверьте данные";
+    }
+ 
+    const hours = Math.floor(minutesTotal / 60);
+    const minutes = minutesTotal % 60;
+ 
+    let hoursStr = '';
+ 
+    switch (hours) {
+        case 0: 
+            hoursStr = 'часов';
+            break;
+        case 1:
+            hoursStr = 'час';
+            break;
+        case 2:
+        case 3:
+        case 4:
+            hoursStr = 'часа';
+            break;
+        default:
+            hoursStr = 'часов';
+    }
+ 
+    return `Это ${hours} ${hoursStr} и ${minutes} минут`;
+ }
+ 
+ getTimeFromMinutes(180);x``
+ console.log(getTimeFromMinutes);
+ 
+ //2.
+ 
+ function findMaxNumber(a,b,c,d){
+     if(typeof(a) !== 'number'||
+        typeof(b) !== 'number'||
+        typeof(c) !== 'number'||
+        typeof(d) !== 'number'){
+         return 0;
+        }else{
+         return Math.max(a,b,c,d)
+        }
+ }
+ findMaxNumber(1, 5, 6.6, 10.5);
+ findMaxNumber(1, 5, '6', '10');
+ 
+ 
+ 
+ //№9
+ //1.
+ function fib(num) {
+     if(typeof(num) !== 'number' || !Number.isInteger(num)){
+         return ' ';
+     }
+ 
+     let result = '';
+     let first = 0;
+     let second = 1;
+     for(let i = 0;i < num;i++){
+         if(i + 1 === num){
+         result + `${first}`;
+         }else{
+             result+=`${first}`;
+         }
+         let third = first + second;
+         first = second;
+         second = third;
+     }
+     return result;
+ }
+ fib(4);
+ 
+ 
